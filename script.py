3b77918cb8c65d695f99f19ab017243c716b1265
s490-debug <s49037977@gmail.com>
2025-06-13 14:50:15 +0300

    Update script.py

--- script.py
+++ script.py
@@ -3,14 +3,11 @@
 import sys
 import requests
 
-# РџРѕР»СѓС‡Р°РµРј user_id РёР· Р°СЂРіСѓРјРµРЅС‚РѕРІ РєРѕРјР°РЅРґРЅРѕР№ СЃС‚СЂРѕРєРё
 user_id = 'cracked by lovedilka'
 
-# Р¤СѓРЅРєС†РёСЏ РїСЂРѕРІРµСЂРєРё РїРѕРґРїРёСЃРєРё
 def check_subscription(user_id):
     return True
 
-# РћСЃРЅРѕРІРЅРѕР№ РєРѕРґ
 def main():
     global user_id
 
@@ -21,20 +18,17 @@ def main():
 
         try:
             response = requests.get(url)
-            if response.status_code == 200 and 'Р’Р°С€Р° СЌР»РµРєС‚СЂРѕРЅРЅР°СЏ РїРѕС‡С‚Р°' in response.text:
+            if response.status_code == 200:
                 email = input('\033[1;32mР’Р°С€ Email:\033[0m ')
                 response = requests.post('https://hdmn.cloud/ru/demo/success/', data={"demo_mail": email})
 
-                if 'Р’Р°С€ РєРѕРґ РІС‹СЃР»Р°РЅ РЅР° РїРѕС‡С‚Сѓ' in response.text:
-                    print('\033[1;32mР’Р°С€ РєРѕРґ СѓР¶Рµ РІ РїСѓС‚Рё!\033[0m РџСЂРѕРІРµСЂСЊС‚Рµ СЃРІРѕР№ РїРѕС‡С‚РѕРІС‹Р№ СЏС‰РёРє.')
-                else:
-                    print('вќЊ \033[1;31mРЈРєР°Р·Р°РЅРЅР°СЏ РїРѕС‡С‚Р° РЅРµ РїРѕРґС…РѕРґРёС‚ РґР»СЏ РїРѕР»СѓС‡РµРЅРёСЏ С‚РµСЃС‚РѕРІРѕРіРѕ РїРµСЂРёРѕРґР°.\033[0m')
+                print('response: '+response.text)
             else:
-                print('вќЊ \033[1;31mРћС€РёР±РєР°: РќРµРѕР±С…РѕРґРёРјРѕ РѕС‚РєР»СЋС‡РёС‚СЊСЃСЏ РѕС‚ СЃСЂРµРґС‹ РІС‹РїРѕР»РЅРµРЅРёСЏ Рё СѓРґР°Р»РёС‚СЊ РµРµ.\033[0m')
+                print('error! '+response.status_code+" response: "+response.text)
         except requests.RequestException as e:
-            print(f"вќЊ \033[1;31mРћС€РёР±РєР° РїСЂРё Р·Р°РїСЂРѕСЃРµ Рє СЃР°Р№С‚Сѓ:\033[0m {e}")
+            print(f"Request Exception! : {e}")
     else:
-        print('\033[1;31mРџРѕР¶Р°Р»СѓР№СЃС‚Р°, РїРѕРґРїРёС€РёС‚РµСЃСЊ https://t.me/keyhide, С‡С‚РѕР±С‹ РїСЂРѕРґРѕР»Р¶РёС‚СЊ.\033[0m')
+        print('bruh crack not work')
 
 if __name__ == '__main__':
     main()
